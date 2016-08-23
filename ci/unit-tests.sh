f7f48dd742f7681493fba5be74ccb1bbd352f668
Josh Hill <jhill@pivotal.io>
2016-08-23 16:14:07 +0100

    remove vendor directory
    it does not play nicely with projects that themselves do not vendor this library
    
    Signed-off-by: Craig Furman <cfurman@pivotal.io>

--- ci/unit-tests.sh
+++ ci/unit-tests.sh
@@ -1,9 +1,12 @@
 #! /bin/bash -eu
 
 export GOPATH=$(pwd)
+export PATH=$GOPATH/bin:$PATH
 
 mkdir -p src/github.com/pivotal-cf/
 ln -s $(pwd)/on-demand-service-broker-sdk src/github.com/pivotal-cf/
 cd src/github.com/pivotal-cf/on-demand-service-broker-sdk
 
+go get -v github.com/tools/godep
+godep restore
 scripts/run-tests.sh
