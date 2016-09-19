65e86c2e6c2e8d0b6521a4f0298ac71d101565f9
Josh Hill <jhill@pivotal.io>
2016-09-19 10:38:16 +0100

    Update ci scripts to use new sdk location

--- ci/unit-tests.sh
+++ ci/unit-tests.sh
@@ -4,8 +4,8 @@ export GOPATH=$(pwd)
 export PATH=$GOPATH/bin:$PATH
 
 mkdir -p src/github.com/pivotal-cf/
-ln -s $(pwd)/on-demand-service-broker-sdk src/github.com/pivotal-cf/
-cd src/github.com/pivotal-cf/on-demand-service-broker-sdk
+ln -s $(pwd)/on-demand-services-sdk src/github.com/pivotal-cf/
+cd src/github.com/pivotal-cf/on-demand-services-sdk
 
 go get -v github.com/tools/godep
 godep restore
