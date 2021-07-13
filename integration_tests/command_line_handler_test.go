53eb57927f1e348a0bebeb7aef2ae185ab4bfaba
Felisia Martini <fmartini@pivotal.io>
2021-07-13 17:16:51 +0100

    Update expectation for executable name after [gomega](https://github.com/onsi/gomega/commit/735636028d705d6a2442a661980253ad6d080d09) change

--- integration_tests/command_line_handler_test.go
+++ integration_tests/command_line_handler_test.go
@@ -199,7 +199,7 @@ var _ = Describe("Command line handler", func() {
 
 				Expect(exitCode).To(Equal(1))
 				Expect(stderr.String()).To(MatchRegexp(
-					`Missing arguments for generate-manifest. Usage: testharness-\d* generate-manifest <service-deployment-JSON> <plan-JSON> <request-params-JSON> <previous-manifest-YAML> <previous-plan-JSON>`))
+					`Missing arguments for generate-manifest. Usage: testharness generate-manifest <service-deployment-JSON> <plan-JSON> <request-params-JSON> <previous-manifest-YAML> <previous-plan-JSON>`))
 			})
 
 			It("exits 1 and logs when a generic error occurs", func() {
@@ -328,7 +328,7 @@ var _ = Describe("Command line handler", func() {
 
 				Expect(exitCode).To(Equal(1))
 				Expect(stderr.String()).To(MatchRegexp(
-					`Missing arguments for create-binding. Usage: testharness-\d* create-binding <binding-ID> <bosh-VMs-JSON> <manifest-YAML> <request-params-JSON>`,
+					`Missing arguments for create-binding. Usage: testharness create-binding <binding-ID> <bosh-VMs-JSON> <manifest-YAML> <request-params-JSON>`,
 				))
 			})
 
@@ -424,8 +424,9 @@ var _ = Describe("Command line handler", func() {
 				exitCode = startPassingCommandAndGetExitCode([]string{"delete-binding", "arg1"})
 
 				Expect(exitCode).To(Equal(1))
+
 				Expect(stderr.String()).To(MatchRegexp(
-					`Missing arguments for delete-binding. Usage: testharness-\d* delete-binding <binding-ID> <bosh-VMs-JSON> <manifest-YAML> <request-params-JSON>`,
+					`Missing arguments for delete-binding. Usage: testharness delete-binding <binding-ID> <bosh-VMs-JSON> <manifest-YAML> <request-params-JSON>`,
 				))
 			})
 
@@ -513,7 +514,7 @@ var _ = Describe("Command line handler", func() {
 
 				Expect(exitCode).To(Equal(1))
 				Expect(stderr.String()).To(MatchRegexp(
-					`Missing arguments for dashboard-url. Usage: testharness-\d* dashboard-url <instance-ID> <plan-JSON> <manifest-YAML>`,
+					`Missing arguments for dashboard-url. Usage: testharness dashboard-url <instance-ID> <plan-JSON> <manifest-YAML>`,
 				))
 			})
 
